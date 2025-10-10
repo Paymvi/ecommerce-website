f00a44038184b3382128180e281153fedc405623
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-10-10 15:51:52 -0400

    Finished 1st section of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -11,6 +14,25 @@ export default async function Home() {
   console.log(products);
 
 
-  
-  return <div></div>;
+
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2> Welcome to My Ecommerce</h2>
+            <p> Discover the latest products at the best prices.</p>
+            <Button asChild variant="default">
+              <Link href="\products">Browse all products</Link>
+            </Button>
+          </div>
+
+          <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]}/>
+
+        </div>
+      </section>
+
+    </div>
+  );
+
 }
