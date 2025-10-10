8844663126f806acc5028abdd3a87ba4f3298661
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-10-10 14:28:52 -0400

    Next.js Boilerplate

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+
+export const Navbar = () => {
+
+    return (
+        <nav>
+            <div>
+                <Link href="/">
+                My Ecommerce
+                </Link>
+            </div>
+        </nav>
+    )
+
+
+}
