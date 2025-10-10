47bf77ced487b9f1df1845338837a755fc9c775e
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-10-10 16:14:44 -0400

    Updated CSS

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Carousel } from "@/components/carousel";
 
 
 export default async function Home() {
@@ -17,19 +18,43 @@ export default async function Home() {
 
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2> Welcome to My Ecommerce</h2>
-            <p> Discover the latest products at the best prices.</p>
-            <Button asChild variant="default">
-              <Link href="\products">Browse all products</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl"> 
+              Welcome to My Ecommerce
+            </h2>
+
+            <p className="text-neutral-600">
+              Discover the latest products at the best prices.
+            </p>
+
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse all products
+              </Link>
+
+
+
             </Button>
           </div>
 
           <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]}/>
 
         </div>
+      </section>
+
+      <section className="py-8">
+        <Carousel/>
       </section>
 
     </div>
