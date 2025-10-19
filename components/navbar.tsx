14f46cb3867a18ebd71181f0b80d11e117357a23
Ishmael Kwayisi <ishmael.kwayisi@snhu.edu>
2025-10-18 21:02:10 -0400

    Added Mobile Menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,28 @@
-import Link from "next/link";
+"use client"
 
+import { ShoppingCartIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useCartStore } from "@/store/cart-store";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export const Navbar = () => {
 
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+    const {items} = useCartStore();
+    const cartCount = items.reduce((acc, item) => acc + item.quantity, 0)
+    
+    useEffect(() => {
+        const handleResize = () =>{
+            if(window.innerWidth >= 768){
+                setMobileOpen(false);
+            }
+        }
+
+        window.addEventListener("resize", handleResize);
+        
+    }, [])
+    
     return (
         <nav className="sticky top-0 z-50 bg-white shadow">
             <div className="container mx-auto flex items-center justify-between px-4 py-4">
@@ -22,11 +42,44 @@ export const Navbar = () => {
                 </Link>
             </div>
 
-            <div className="flex items-center space-x-4"></div>
+            <div className="flex items-center space-x-4">
+                <Link href="/checkout">
+                    <ShoppingCartIcon className="h-6 w-6" />
+                    {cartCount > 0 && <span> {cartCount} </span>}
+                </Link>
+                <Button 
+                    variant="ghost" 
+                    className="md:hidden"
+                    onClick={() => setMobileOpen((prev) => !prev)}
+                >
+                    {mobileOpen ? (<XMarkIcon />) : <Bars3Icon />}
+                </Button>
             </div>
+            </div>
+            {mobileOpen && (
+                <nav className="md:hidden bg-white shadow-md">
+                    {" "}
+                    <ul className="flex flex-col p-4 space-y-2">
+                    {" "}
+                        <li>
+                            <Link href="/" className="block hover:text-blue-600"> 
+                                Home 
+                            </Link>
+                        </li>
+                                                <li>
+                            <Link href="/products" className="block hover:text-blue-600">  
+                                Products 
+                            </Link>
+                        </li>
+                                                <li>
+                            <Link href="/checkout" className="block hover:text-blue-600">  
+                                Checkout 
+                            </Link>
+                        </li>
+                    </ul>
+
+                </nav>
+            )}
         </nav>
-        );
-
-
-
+    );
 }
